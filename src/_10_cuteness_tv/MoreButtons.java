f61a19dae430274ed49eb32a84e2574f684f6394
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2021-03-29 18:27:46 -0700

    insertMessageHere

--- src/_10_cuteness_tv/MoreButtons.java
+++ src/_10_cuteness_tv/MoreButtons.java
@@ -1,8 +1,53 @@
 package _10_cuteness_tv;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URI;
 
-public class MoreButtons {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class MoreButtons implements ActionListener {
+	JFrame GUI = new JFrame();
+	 JButton ghost = new JButton();
+	 JButton ghast = new JButton();
+	 JButton ghoul = new JButton();
+	public void space() {
+		GUI.setVisible(true);
+ghost.setText("duck");
+ghost.addActionListener(this);
+ghast.setText("frog");
+ghast.addActionListener(this);
+ghoul.setText("unicorns");
+ghoul.addActionListener(this);
+JPanel formatter = new JPanel();
+			formatter.add(ghost);
+			formatter.add(ghast);
+		    formatter.add(ghoul);
+			GUI.add(formatter);
+		    GUI.pack();
+				
+ }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		System.out.println("?");
+		if (buttonPressed.equals(ghost)) {
+	showDucks();
+	System.out.println("goose");
+		}
+		else if (buttonPressed.equals(ghast)) {
+			showFrog();
+			System.out.println("toad");
+				}
+		else if (buttonPressed.equals(ghoul)) {
+			showFluffyUnicorns();
+			System.out.println("horse");
+				}
+	}
 	void showDucks() {
 	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
 	}
@@ -32,5 +77,6 @@ public class MoreButtons {
 	          e.printStackTrace();
 	     }
 	}
+	
 
 }
